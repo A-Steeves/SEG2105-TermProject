9768bed785322ecf82da9130ba3fa9a9176bfc6f
ajay-mundi <65574040+ajay-mundi@users.noreply.github.com>
2021-06-15 18:59:26 -0400

    Added simple listview to user page

--- app/src/main/java/com/example/lab3/AdminFragment2.java
+++ app/src/main/java/com/example/lab3/AdminFragment2.java
@@ -4,22 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class AdminFragment2 extends Fragment {
-
+ListView listView;
     @Nullable
     @org.jetbrains.annotations.Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        //recyclerView = recyclerView.findViewById(R.id.recycle);
-        //listView = listView.findViewById(R.id.listview);
         View v = inflater.inflate(R.layout.fragment_admin2,container,false);
-        //View v = inflater.inflate(R.layout.list_items,container,false);
+        listView = v.findViewById(R.id.listviewUser);
 
-        //setAdapter();
+        String[] display = {"Ajay", "Manvir", "Alex"};
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1,display);
+        listView.setAdapter(listViewAdapter);
+        //listView.setAdapter(adapter);
         return v;
     }
 
